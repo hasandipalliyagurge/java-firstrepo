4f3aea2e8580bc5920ac0639fab399db5310d1d9
hasandi_palliaguruge <pghasandi@gmail.com>
2019-10-19 23:29:26 +0530

    logical operations

---
+++ Ternary.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Ternary{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input st average : ");
+		double avg=input.nextDouble();
+		
+		/*
+		if(avg>=50){
+			System.out.println("Pass");
+		}else{
+			System.out.println("Fail");
+		}*/
+		
+		
+		
+		
+		
+		
+		System.out.println(avg>=50 ? "Pass":"Fail");
+		
+	}
+}
