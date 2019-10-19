041e6bdd2c1f514a2ca2b341ca07679e14bb6495
hasandi_palliaguruge <pghasandi@gmail.com>
2019-10-19 23:25:07 +0530

    Arithmetic operations

---
+++ Arithmetic1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Arithmetic1{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		int x=10;
+		
+		    System.out.println(x); //100
+			System.out.println(-x); //-100
+			System.out.println(x); //100
+		}
+	}
+
+	
