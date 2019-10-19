05520c5e7acb715c2c3b94744603553d89723b07
hasandi_palliaguruge <pghasandi@gmail.com>
2019-10-19 23:30:05 +0530

    logical operations

---
+++ Unary.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Unary{
+	public static void main(String args[]){
+		int x=100;
+		System.out.println(x);
+		
+		x++;
+		System.out.println(x); //101
+		
+		++x;
+		System.out.println(x);//102
+		
+		x--;
+		System.out.println(x);//101
+		
+		--x;
+		System.out.println(x);//100
+	}
+}
