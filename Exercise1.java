fcb80b56028ab0cdfe0f6f7cb1058ba178f3dc8d
hasandi_palliaguruge <pghasandi@gmail.com>
2019-11-02 21:44:16 +0530

    variable scope

---
+++ Exercise1.java
@@ -0,0 +1,15 @@
+class Exercise1 {
+    public static void main(String args[]) {
+		System.out.print("[");
+		int i=0;
+        for(; i<10; i++){	
+			System.out.print(i+", ");	
+		}
+		
+		 
+	System.out.println("\b\b]");	
+	
+	
+    }
+}
+
