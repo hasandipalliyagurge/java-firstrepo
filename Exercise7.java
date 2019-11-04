6ecf04ce92aea74bb62e4e8b7727ef6ea4338657
hasandi_palliaguruge <pghasandi@gmail.com>
2019-11-04 21:43:48 +0530

    code blocks

---
+++ Exercise7.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Exercise7 {
+    public static void main(String args[]) {
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input number- ");
+		int n=input.nextInt();
+		
+		if(n==0){
+			System.out.println("[Empty]");
+		}
+		
+		else{
+		System.out.print("[");
+		for (int i = 0; i < n; i++)
+		{
+			System.out.print(n);
+		}
+		 
+		System.out.println("]");
+		
+	}
+		
+		
+		
+		
+		
+		
+}
+
+}
+
