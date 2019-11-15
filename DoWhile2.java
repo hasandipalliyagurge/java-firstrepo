5097dd2f5408176bb191f5b93c4c3469bc26639f
hasandi_palliaguruge <pghasandi@gmail.com>
2019-11-15 22:58:27 +0530

    do while loop

---
+++ DoWhile2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class DoWhile2 {
+    public static void main(String args[]) {
+        int i=0;
+        while(i<10){
+			System.out.println("while-loop : "+i);
+			i++;
+		}
+		//---------------------------------------
+		System.out.println("--------------------");
+		int j=0;
+		do{
+			System.out.println("do/while-loop : "+j);
+			j++;
+		}while(j<10);
+			
+    }
+}
