6db5ea8412e3b06e649593e58e538dded237ecf7
hasandi_palliaguruge <pghasandi@gmail.com>
2019-11-18 22:28:54 +0530

    Nested for loop

---
+++ NestedL2.java
@@ -0,0 +1,12 @@
+class NestedL2 {
+    public static void main(String args[]) {
+       for(int i=10; i>0; i--){
+		   for(int j=10; j>0; j--){
+			   System.out.println(i+" "+j);
+		   }
+	   }
+		   
+     
+    }
+}
+
