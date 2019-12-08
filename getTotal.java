88b63a82a1d58d3bce1e29414bee4ee1dee9adce
hasandi_palliaguruge <pghasandi@gmail.com>
2019-12-08 22:18:44 +0530

    total

---
+++ getTotal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class getTotal {
+	
+	public static int getTotal(int a, int b){
+		int total;
+		total=a+b;
+		return total;
+	}
+	
+	//Insert method declaration
+	public static void main(String args[]) {
+		int tot;
+		tot=getTotal(12,13);
+		System.out.println("Tot : "+tot);
+	}
+}
+
