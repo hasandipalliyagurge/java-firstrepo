be8c6c83cb583b35e564c6d2db1aecea087677f9
hasandi_palliaguruge <pghasandi@gmail.com>
2019-12-13 21:45:11 +0530

    reverse

---
+++ Reverse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Example {
+	public static void reverse(int[] x){
+		
+	}
+	public static void main(String args[]) {
+		int[] a={12,90,45,78,24,80,14,82,56,77,19,33};
+		System.out.println(Arrays.toString(a));
+		reverse(a);
+		System.out.println(Arrays.toString(a));
+	}
+}
